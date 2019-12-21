9b837d7d1bb3f9e331e3384575352f29ba1e1ef1
Ismael Pérez <ismaelprzm@gmail.com>
2019-12-21 23:02:59 +0100

    Format code

--- Labs/Lab1.3/forum/dm_local.js
+++ Labs/Lab1.3/forum/dm_local.js
@@ -1,57 +1,55 @@
-var dm = require ('./dm.js');
+var dm = require("./dm.js");
 
 // true if already exists
-exports.addUser = function (u,p, cb) {
-	var exists = dm.addUser(u,p);
-	cb (exists);
-}
+exports.addUser = function(u, p, cb) {
+  var exists = dm.addUser(u, p);
+  cb(exists);
+};
 
 // Adds a new subject to subject list. Returns -1 if already exists, id on success
-exports.addSubject = function (s, cb) {
-	var id = dm.addSubject (s);
-	cb (id);
-}
+exports.addSubject = function(s, cb) {
+  var id = dm.addSubject(s);
+  cb(id);
+};
 
-exports.getSubjectList = function (cb) {
-	var list = dm.getSubjectList ();
-	cb (list);
-}
+exports.getSubjectList = function(cb) {
+  var list = dm.getSubjectList();
+  cb(list);
+};
 
-exports.getUserList = function (cb) {
-	var list = dm.getUserList ();
-	cb (list);
-}
+exports.getUserList = function(cb) {
+  var list = dm.getUserList();
+  cb(list);
+};
 
 // Tests if credentials are valid, returns true on success
-exports.login = function (u, p, cb) {
-	var ok = dm.login (u,p);
-	cb (ok);
-}
+exports.login = function(u, p, cb) {
+  var ok = dm.login(u, p);
+  cb(ok);
+};
 
-exports.addPrivateMessage = function (msg, cb){
-	dm.addPrivateMessage (msg);
-	cb();
-}
+exports.addPrivateMessage = function(msg, cb) {
+  dm.addPrivateMessage(msg);
+  cb();
+};
 
-exports.getPrivateMessageList = function (u1, u2, cb) {
-	var list = dm.getPrivateMessageList (u1,u2);
-	cb (list);
-}
+exports.getPrivateMessageList = function(u1, u2, cb) {
+  var list = dm.getPrivateMessageList(u1, u2);
+  cb(list);
+};
 
-function getSubject (sbj, cb) {
-	var id = dm.getSubject (sbj);
-	cb (id);
+function getSubject(sbj, cb) {
+  var id = dm.getSubject(sbj);
+  cb(id);
 }
 
 // adds a public message to storage
-exports.addPublicMessage = function (msg, cb)
-{
-	dm.addPublicMessage (msg);
-	cb ();
-}
+exports.addPublicMessage = function(msg, cb) {
+  dm.addPublicMessage(msg);
+  cb();
+};
 
-exports.getPublicMessageList = function (sbj, cb) {
-	var list = dm.getPublicMessageList (sbj);
-	cb (list);
-}
-
+exports.getPublicMessageList = function(sbj, cb) {
+  var list = dm.getPublicMessageList(sbj);
+  cb(list);
+};
--- Labs/Lab1.3/forum/dmclient.js
+++ Labs/Lab1.3/forum/dmclient.js
@@ -1,13 +1,12 @@
-var dm = require ('./dm_remote.js');
+var dm = require("./dm_remote.js");
 
-var HOST = '127.0.0.1';
+var HOST = "127.0.0.1";
 var PORT = 9000;
 
-
-dm.Start(HOST, PORT, function () {
-    	// Write the command to the server 
-   	dm.getSubjectList (function (ml) {
-   		console.log ("here it is:")
-   		console.log (JSON.stringify(ml));
-   	});
+dm.Start(HOST, PORT, function() {
+  // Write the command to the server
+  dm.getSubjectList(function(ml) {
+    console.log("here it is:");
+    console.log(JSON.stringify(ml));
+  });
 });
