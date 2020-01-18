e319e7abe61c16389a615d084f5bcaeaff0f0719
Ismael Pérez <ismaelprzm@gmail.com>
2020-01-18 22:05:04 +0100

    Add resources provided to complete the laboratory

---
+++ Labs/Lab2.2/forum/dmclient.js
@@ -0,0 +1,86 @@
+var dm = require("./dm_remote.js");
+
+var args = process.argv.slice(2);
+
+// Extract the host and port args if exists
+var input = process.argv[2];
+var inputPort;
+var inputHost;
+if (input && input.includes(":")) {
+  inputHost = input.split(":")[0];
+  inputPort = input.split(":")[1];
+  args = process.argv.slice(3);
+}
+
+// Set port and host values depending on the input args
+const PORT = process.env.PORT || inputPort || 9000;
+const HOST = inputHost || "127.0.0.1";
+
+dm.Start(HOST, PORT, function() {
+  // Write the command to the server
+  switch (args[0]) {
+    case "get subject list":
+      dm.getSubjectList(function(ml) {
+        console.log("here it is:");
+        console.log(JSON.stringify(ml));
+      });
+      break;
+    case "get user list":
+      dm.getUserList(function(ml) {
+        console.log("here it is:");
+        console.log(JSON.stringify(ml));
+      });
+      break;
+    case "get public message list":
+      dm.getPublicMessageList(args[1], function(ml) {
+        console.log("here it is:");
+        console.log(JSON.stringify(ml));
+      });
+      break;
+    case "get private message list":
+      dm.getPrivateMessageList(args[1], args[2], function(ml) {
+        console.log("here it is:");
+        console.log(JSON.stringify(ml));
+      });
+      break;
+    case "login":
+      dm.login(args[1], args[2], function(ml) {
+        console.log("here it is:");
+        console.log(JSON.stringify(ml));
+      });
+      break;
+    case "add user":
+      dm.addUser(args[1], args[2], function(ml) {
+        console.log("here it is:");
+        console.log(JSON.stringify(ml));
+      });
+      break;
+    case "add subject":
+      dm.addSubject(args[1], function(ml) {
+        console.log("here it is:");
+        console.log(JSON.stringify(ml));
+      });
+      break;
+    case "add public message":
+      dm.addPublicMessage(args[1], function(ml) {
+        console.log("here it is:");
+        console.log(JSON.stringify(ml));
+      });
+      break;
+    case "publish public message":
+      var msjJson = JSON.parse(args[1]);
+      dm.publishPublicMessage(msjJson, function() {
+        console.log("Msg: " + args[1] + " published succesfully\n");
+      });
+      break;
+    case "add private message":
+      dm.addPrivateMessage(args[1], function(ml) {
+        console.log("here it is:");
+        console.log(JSON.stringify(ml));
+      });
+      break;
+    default:
+      console.log("Invalid option");
+      break;
+  }
+});
